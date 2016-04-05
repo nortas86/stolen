372d317f4e16dd0830dddaf84e40ba718aea90c7
Vasilis <Vasilis@Jarvis>
2016-04-05 12:32:58 +0300

    update UserInformation forms

--- src/AppBundle/Form/UserInformationType.php
+++ src/AppBundle/Form/UserInformationType.php
@@ -15,7 +15,6 @@ class UserInformationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('id')
             ->add('first_name')
             ->add('last_name')
             ->add('language')
